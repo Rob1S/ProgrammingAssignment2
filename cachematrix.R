ed9c1edae524ba35a1653e732cd5aa53dd431485
Rob1S <Rob1S@users.noreply.github.com>
2014-07-27 18:32:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function creates a chache "matrix" object of the inverse of matrix "x".
+## It is assumed that matrix "x" is always invertible
+## The inverse is "stored" in object inv
+## The matrix x is "stored" in object src, so we can check if it is changed or not.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <<- solve(x)
+        src <<- x
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of given "matrix".
+## If the matrix has not changed and the inverse has already been calculated, 
+## then the cachesolve should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## First check if x is not changed
+        check <- is.matrix(x) && is.matrix(src) && dim(x) == dim(src) && all(x == src)
+## Then check if the invertion has been done
+        if (check)  
+                if(!is.null(inv)) {
+## Return a matrix that is the inverse of 'x'
+                message("getting cached data")
+                return(inv)
+        }
+## Else (if x has changed or the inversion has not taken place, inverse x 
+        inv <<- solve(x)
+        return(inv)
 }
